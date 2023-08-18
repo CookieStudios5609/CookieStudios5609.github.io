5c1d69404b80102e87575daedb030a47da429224
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2023-08-18 19:47:14 -0400

    Just tear it all down!

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-Don't look at this, you'll regret it.
-
-and yes, I load a >20mb gif file instead of using something compressed or a lottie or some alternative
+Oh fun, oh very fun
--- index.js
+++
@@ -1,6 +0,0 @@
-function enableLightMode() {
-    var body = document.body;
-    document.getElementById("ghublink").style.color = "172f55";
-    body.classList.toggle("light-mode");
-
-}
